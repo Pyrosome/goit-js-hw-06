dff5deae495d9630fd7d21f8e379f47c7cb7e255
Gaultheria <valeriaburina@gmail.com>
2023-01-02 03:14:01 +0100

    task 6 v2

--- js 16.00.55/task-06.js
+++ js 16.00.55/task-06.js
@@ -6,10 +6,18 @@
 
 const input = document.querySelector("#validation-input");
 
+// input.addEventListener("blur", () => {
+//   if (input.value.length === Number(input.dataset.length)) {
+//     input.classList.add("valid");
+//     input.classList.remove("invalid");
+//   } else {
+//     input.classList.add("invalid");
+//     input.classList.remove("valid");
+//   }
+// });
+
 input.addEventListener("blur", () => {
-  if (input.value.length === Number(input.dataset.length)) {
-    input.classList.add("valid");
-    input.classList.remove("invalid");
-  } else input.classList.add("invalid");
-  input.classList.remove("valid");
+  if (input.value.length !== Number(input.dataset.length)) {
+    input.classList.add(`invalid`);
+  } else input.classList.replace(`invalid`, `valid`);
 });
