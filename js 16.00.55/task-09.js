b03d0cd9823f524839f9548a65cb0216414d3dc6
Gaultheria <valeriaburina@gmail.com>
2023-01-03 02:59:39 +0100

    task 9

--- js 16.00.55/task-09.js
+++ js 16.00.55/task-09.js
@@ -1,3 +1,14 @@
+// Напиши скрипт, который изменяет цвета фона элемента < body > через инлайн стиль при клике
+// на button.change - color и выводит значение цвета в span.color.
+
+const button = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+
+button.addEventListener("click", (event) => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = `${getRandomHexColor()}`;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
